3bc2852b9351d390591d7878dedd11f5a1a9097c
Riham <riham.aljlelatie@gmail.com>
2025-01-21 19:48:26 +0300

    add request notifications permission for high android

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:timers/routes.dart';
 
 import 'notification_services.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.notification.isDenied.then((value) {
+    if (value) {
+      Permission.notification.request();
+    }
+  });
 
   await NotificationService.initialize();
 
